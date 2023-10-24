5d50661894dc372d947cfb59f8a4068daaaf0a5c
I552408 <jan.valtmann@sap.com>
2023-10-24 17:46:04 +0200

    adjust login and register

--- app/test_application/lib/login.dart
+++ app/test_application/lib/login.dart
@@ -1,7 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:test_application/customPageRouteBuilder.dart';
 import 'package:test_application/start.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-class Login extends StatelessWidget {
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  Future<void> performLogin() async {
+    final url = Uri.parse('http://10.0.2.2:8080/api/auth/signin');
+    final response = await http.post(
+      url,
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({
+        'username': emailController.text,
+        'password': passwordController.text,
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      // Login was successful, you can navigate to the next page.
+      Navigator.push(
+        context,
+        CustomPageRouteBuilder.slideInFromRight(Start()),
+      );
+      print("Success");
+    } else {
+      // Handle the error or show a message to the user.
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,12 +48,14 @@ class Login extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             TextFormField(
+              controller: emailController,
               decoration: const InputDecoration(
-                labelText: 'E-Mail',
+                labelText: 'Username',
               ),
             ),
             const SizedBox(height: 16),
             TextFormField(
+              controller: passwordController,
               obscureText: true,
               decoration: const InputDecoration(
                 labelText: 'Passwort',
@@ -27,8 +63,9 @@ class Login extends StatelessWidget {
             ),
             const SizedBox(height: 32),
             ElevatedButton(
-              onPressed: () => Navigator.push(
-                  context, MaterialPageRoute(builder: (context) => Start())),
+              onPressed: () {
+                performLogin(); // Call the login function
+              },
               child: const Text('Login'),
             ),
           ],
--- app/test_application/lib/register.dart
+++ app/test_application/lib/register.dart
@@ -1,6 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:test_application/customPageRouteBuilder.dart';
+import 'dart:convert';
 
-class Register extends StatelessWidget {
+import 'package:test_application/login.dart';
+
+class Register extends StatefulWidget {
+  @override
+  _RegisterState createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  final TextEditingController usernameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController repeatPasswordController =
+      TextEditingController();
+
+  Future<void> performRegistration() async {
+    final url = Uri.parse('http://10.0.2.2:8080/api/auth/signup');
+    final response = await http.post(
+      url,
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({
+        'username': usernameController.text,
+        'email': emailController.text,
+        'password': passwordController.text,
+        'role': ['user']
+      }),
+    );
+
+    print(response.body);
+
+    if (response.statusCode == 200) {
+      //TODO: Register success popup
+      Navigator.push(
+        context,
+        CustomPageRouteBuilder.slideInFromRight(Login()),
+      );
+    } else {
+      // Handle the error or show a message to the user.
+      print("Error");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,28 +56,37 @@ class Register extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             TextFormField(
+              controller: usernameController,
+              decoration: const InputDecoration(
+                labelText: 'Username',
+              ),
+            ),
+            TextFormField(
+              controller: emailController,
               decoration: const InputDecoration(
                 labelText: 'E-Mail',
               ),
             ),
             const SizedBox(height: 16),
             TextFormField(
+              controller: passwordController,
               obscureText: true,
               decoration: const InputDecoration(
-                labelText: 'Passwort',
+                labelText: 'Password',
               ),
             ),
             const SizedBox(height: 16),
             TextFormField(
+              controller: repeatPasswordController,
               obscureText: true,
               decoration: const InputDecoration(
-                labelText: 'Passwort wiederholen',
+                labelText: 'Repeat Password',
               ),
             ),
             const SizedBox(height: 32),
             ElevatedButton(
               onPressed: () {
-                // Implement your registration logic here
+                performRegistration(); // Call the registration function
               },
               child: const Text('Registrieren'),
             ),
