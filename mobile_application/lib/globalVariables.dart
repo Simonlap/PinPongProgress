7d4bd900bd9ea9a9f86229e174ddb98831c4e45c
Simon Lappe <simon.lappe@sap.com>
2024-01-20 20:31:51 +0100

    start and login page

---
+++ mobile_application/lib/globalVariables.dart
@@ -0,0 +1,9 @@
+String? username = null;
+String? useremail = null;
+int? userid = null;
+List<dynamic>? userroles = null;
+String? jwtToken = null;
+
+//List<Player> player = [];
+
+String apiUrl = 'http://10.0.2.2:8080';
