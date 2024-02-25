c1ab38dbf270dd81dc0626282fcf3835ca64c46c
Simon Lappe <simon.lappe@sap.com>
2024-02-25 18:02:39 +0100

    adaptation to statistics page

--- mobile_application/lib/pages/playerEloRating_page.dart
+++ mobile_application/lib/pages/playerEloRating_page.dart
@@ -1,19 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:charts_flutter/flutter.dart' as charts;
-import 'package:mobile_application/entities/eloRating.dart';
-import 'package:mobile_application/globalVariables.dart'; // Adjust the import path as necessary
+import 'dart:math' as Math;
+import 'package:mobile_application/entities/eloRating.dart'; // Adjust the import path as necessary
 import 'package:mobile_application/entities/player.dart';
+import 'package:mobile_application/globalVariables.dart'; // Adjust the import path as necessary
 
 class PlayerEloRatingPage extends StatefulWidget {
   @override
   _PlayerEloRatingPageState createState() => _PlayerEloRatingPageState();
 }
 
-enum Scale { Minutes, Hours, Days }
-
 class _PlayerEloRatingPageState extends State<PlayerEloRatingPage> {
   Player? _selectedPlayer;
-  Scale _selectedScale = Scale.Days; // Default scale
 
   @override
   Widget build(BuildContext context) {
@@ -29,7 +27,7 @@ class _PlayerEloRatingPageState extends State<PlayerEloRatingPage> {
               isExpanded: true,
               value: _selectedPlayer,
               hint: Text('Select a Player'),
-              items: player.map((Player player) {
+              items: player.map((Player player) { // Ensure you have a list of players named `players`
                 return DropdownMenuItem<Player>(
                   value: player,
                   child: Text(player.name),
@@ -42,37 +40,41 @@ class _PlayerEloRatingPageState extends State<PlayerEloRatingPage> {
               },
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: DropdownButton<Scale>(
-              value: _selectedScale,
-              onChanged: (Scale? newValue) {
-                setState(() {
-                  _selectedScale = newValue!;
-                });
-              },
-              items: Scale.values.map((Scale scale) {
-                return DropdownMenuItem<Scale>(
-                  value: scale,
-                  child: Text(scale.toString().split('.').last),
-                );
-              }).toList(),
-            ),
-          ),
           Expanded(
             child: _selectedPlayer != null
-                ? _buildEloRatingChart(_selectedPlayer!, _selectedScale)
-                : Center(child: Text('Please select a player and scale')),
+                ? _buildEloRatingChart(_selectedPlayer!)
+                : Center(child: Text('Please select a player')),
           ),
         ],
       ),
     );
   }
 
-  Widget _buildEloRatingChart(Player player, Scale scale) {
-    // Ensure data is sorted by date to prevent lines going back in time
+  Widget _buildEloRatingChart(Player player) {
     List<EloRating> sortedRatings = List<EloRating>.from(player.eloRatings);
     sortedRatings.sort((a, b) => a.date.compareTo(b.date));
+
+    final minElo = sortedRatings.map<double>((rating) => rating.elo.toDouble()).fold(double.infinity, (double previousValue, double elo) => Math.min(previousValue, elo));
+    final maxElo = sortedRatings.map<double>((rating) => rating.elo.toDouble()).fold(-double.infinity, (double previousValue, double elo) => Math.max(previousValue, elo));
+
+    final yAxisStart = minElo - (minElo * 0.05); // Start 5% below the minimum ELO
+    final yAxisEnd = maxElo + (maxElo * 0.05); // End 5% above the maximum ELO
+
+    // Calculate the range of the data
+    final range = yAxisEnd - yAxisStart;
+
+    // Determine tick interval based on range
+    // Adjust these intervals and ranges based on what you consider appropriate for your data
+    double tickInterval;
+    if (range <= 10) {
+      tickInterval = 1; // For very narrow ranges, use small intervals
+    } else if (range <= 50) {
+      tickInterval = 5; // For slightly wider ranges, use larger intervals
+    } else if (range <= 100) {
+      tickInterval = 10; // For ranges within 100, use 10 unit intervals
+    } else {
+      tickInterval = (range / 10).ceilToDouble(); // For larger ranges, scale the interval based on the range
+    }
 
     List<charts.Series<EloRating, DateTime>> series = [
       charts.Series<EloRating, DateTime>(
@@ -84,29 +86,19 @@ class _PlayerEloRatingPageState extends State<PlayerEloRatingPage> {
       )
     ];
 
-    var tickProviderSpec;
-    switch (scale) {
-      case Scale.Minutes:
-        tickProviderSpec = charts.AutoDateTimeTickProviderSpec(
-          includeTime: true,
-        );
-        break;
-      case Scale.Hours:
-        tickProviderSpec = charts.AutoDateTimeTickProviderSpec(
-          includeTime: true,
-        );
-        break;
-      case Scale.Days:
-        tickProviderSpec = charts.AutoDateTimeTickProviderSpec();
-        break;
-    }
-
     return charts.TimeSeriesChart(
       series,
       animate: true,
       dateTimeFactory: const charts.LocalDateTimeFactory(),
       domainAxis: charts.DateTimeAxisSpec(
-        tickProviderSpec: tickProviderSpec,
+        tickProviderSpec: charts.AutoDateTimeTickProviderSpec(), // Default behavior for daily scale
+      ),
+      primaryMeasureAxis: charts.NumericAxisSpec(
+        viewport: charts.NumericExtents(yAxisStart, yAxisEnd),
+        tickProviderSpec: charts.StaticNumericTickProviderSpec(
+          // Generate tick values based on calculated interval
+          List.generate((range / tickInterval).ceil(), (index) => charts.TickSpec(index * tickInterval + yAxisStart, label: '${index * tickInterval + yAxisStart}'))
+        ),
       ),
     );
   }
