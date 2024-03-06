5081e017c3da6632f94a311a4c87978726b78968
I552408 <jan.valtmann@sap.com>
2024-03-06 16:08:03 +0100

    make toast and dialog fancy

--- mobile_application/lib/elements/customAlertDialog.dart
+++ mobile_application/lib/elements/customAlertDialog.dart
@@ -1,27 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_application/elements/customElevatedButton.dart';
 
 class CustomAlertDialog extends StatelessWidget {
   final String title;
   final String message;
+  final List<Widget>? actions; // Optional actions
 
-  CustomAlertDialog({required this.title, required this.message});
+  CustomAlertDialog({
+    required this.title,
+    required this.message,
+    this.actions, // Initialize actions in the constructor
+  });
 
   @override
   Widget build(BuildContext context) {
-    return AlertDialog(
-      title: Text(title),
-      content: Text(message),
-      actions: [
-        TextButton(
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-          child: Text('OK'),
+    return Dialog(
+      elevation: 5,
+      child: Container(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              title,
+              style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+                color: Color(0xFF294597),
+              ),
+            ),
+            SizedBox(height: 16),
+            Text(
+              message,
+              style: TextStyle(
+                fontSize: 16.0,
+                color: Color(0xFF294597),
+              ),
+            ),
+            SizedBox(height: 20),
+            if (actions != null)
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly, // Center buttons horizontally
+                children: actions!,
+              )
+            else
+              CustomElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                text: 'OK',
+              ),
+          ],
         ),
-      ],
+      ),
     );
   }
 }
+
 
 Future<void> showAlert(BuildContext context, String title, String message) async {
   await showDialog(
@@ -43,25 +78,27 @@ Future<bool> showConfirmationDialog({
   await showDialog(
     context: context,
     builder: (BuildContext context) {
-      return AlertDialog(
-        title: Text(title),
-        content: Text(message),
-        actions: <Widget>[
-          TextButton(
+      return CustomAlertDialog(
+        title: title,
+        message: message,
+        actions: [
+          CustomElevatedButton(
             onPressed: () {
               Navigator.of(context).pop();
+              confirmed = false;
               if (onCancel != null) {
                 onCancel();
               }
             },
-            child: Text('Nein'),
+            text: 'Nein',
           ),
-          TextButton(
+          CustomElevatedButton(
             onPressed: () {
+              Navigator.of(context).pop();
               confirmed = true;
               onConfirm();
             },
-            child: Text('Ja'),
+            text: 'Ja',
           ),
         ],
       );
--- mobile_application/lib/elements/customToast.dart
+++ mobile_application/lib/elements/customToast.dart
@@ -2,47 +2,102 @@ import 'package:flutter/material.dart';
 
 class CustomToast {
   static void show(BuildContext context, String message) {
-    OverlayEntry overlayEntry = OverlayEntry(
-      builder: (context) => Positioned(
-        bottom: 70.0, 
-        width: MediaQuery.of(context).size.width, 
-        child: Material(
-          color: Colors.transparent,
-          child: Container(
-            padding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
-            margin: EdgeInsets.symmetric(horizontal: 40.0),
-            decoration: BoxDecoration(
-              color: Color(0xFFFFE019),
-              borderRadius: BorderRadius.circular(8.0),
-            ),
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Flexible(
-                  child: Text(
-                    message,
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 16.0,
-                      color: Color(0xFF294597),
-                      overflow: TextOverflow.clip,
+    OverlayEntry overlayEntry = OverlayEntry(builder: (context) => CustomToastWidget(message: message));
+    Overlay.of(context)!.insert(overlayEntry);
+
+    Future.delayed(Duration(seconds: 3), () {
+      overlayEntry.remove();
+    });
+  }
+}
+
+class CustomToastWidget extends StatefulWidget {
+  final String message;
+
+  const CustomToastWidget({Key? key, required this.message}) : super(key: key);
+
+  @override
+  _CustomToastWidgetState createState() => _CustomToastWidgetState();
+}
+
+class _CustomToastWidgetState extends State<CustomToastWidget> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _fadeAnimation;
+  late Animation<Offset> _positionAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: Duration(milliseconds: 500),
+      vsync: this,
+    );
+
+    _fadeAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(_controller);
+
+    _positionAnimation = Tween<Offset>(
+      begin: Offset(0, 3.0),
+      end: Offset.zero,
+    ).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.fastOutSlowIn,
+    ));
+
+    _controller.forward();
+
+    Future.delayed(Duration(seconds: 2), () {
+      _controller.reverse();
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      bottom: 50.0,
+      child: Material(
+        color: Colors.transparent,
+        child: AnimatedBuilder(
+          animation: _controller,
+          builder: (context, child) => FadeTransition(
+            opacity: _fadeAnimation,
+            child: SlideTransition(
+              position: _positionAnimation,
+              child: Container(
+                width: MediaQuery.of(context).size.width * 0.8,
+                margin: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width * 0.1),
+                padding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
+                decoration: BoxDecoration(
+                  color: Color(0xFFFFE019).withOpacity(0.95),
+                  borderRadius: BorderRadius.circular(25.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.2),
+                      spreadRadius: 1,
+                      blurRadius: 5,
+                      offset: Offset(0, 3),
                     ),
+                  ],
+                ),
+                child: Text(
+                  widget.message,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    color: Color(0xFF294597),
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-              ],
+              ),
             ),
           ),
         ),
       ),
     );
-
-    // Insert the overlay entry into the overlay
-    Overlay.of(context)!.insert(overlayEntry);
-
-    // Remove the overlay after some time
-    Future.delayed(Duration(seconds: 2), () {
-      overlayEntry.remove();
-    });
   }
 }
